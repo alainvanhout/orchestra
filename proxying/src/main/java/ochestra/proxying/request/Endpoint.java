a5b5c3af489c9875611536250933df8fd6de9ed1
Alain Van Hout <alainvanhout@gmail.com>
2016-05-14 20:26:37 +0200

    Endpoint should make use of its requestService

--- proxying/src/main/java/ochestra/proxying/request/Endpoint.java
+++ proxying/src/main/java/ochestra/proxying/request/Endpoint.java
@@ -2,14 +2,7 @@ package ochestra.proxying.request;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import ochestra.proxying.ResourceConversionService;
 import orchestra.instrument.identity.ServiceIdentity;
-import org.apache.commons.io.IOUtils;
-
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class Endpoint {
 
@@ -30,26 +23,7 @@ public class Endpoint {
     }
 
     public JsonResponse send(String body, String path, String method){
-        HttpURLConnection connection = null;
-
-        try {
-            URL obj = new URL(toUrl() + path);
-            connection = (HttpURLConnection) obj.openConnection();
-            connection.setRequestMethod(method);
-            if (body != null){
-                connection.setDoOutput(true);
-                connection.setDoInput(true);
-                connection.setRequestProperty("Content-Type", "application/json");
-                OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
-                writer.write(toJson(body));
-                writer.flush();
-            }
-            connection.getResponseCode();
-            return new JsonResponse(IOUtils.toString(connection.getInputStream()));
-        } catch (IOException e) {
-            return null;
-            // throw exception?
-        }
+        return requestService.perform(this, body, path, method);
     }
 
     public String toJson(Object body) {
